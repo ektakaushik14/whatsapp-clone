860f4209b79aa0ee517443788589a3ca045dc6c8
ekta kaushik <ektakaushik14@gmail.com>
2021-05-19 14:32:16 +0530

    updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,18 +1,18 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton/IconButton";
-import People from "@material-ui/icons/People";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
 import "./sidebar.css";
 import SidebarChat from "./SidebarChat";
+import { Avatar } from "@material-ui/core";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar_header">
-        <People />
+        <Avatar />
         <div className="sidebar-headerright">
           <IconButton>
             <DonutLargeIcon />
@@ -23,7 +23,7 @@ export default function Sidebar() {
           <IconButton>
             <MoreVertIcon />
           </IconButton>
-        </div>
+        </div>{" "}
       </div>
       <div className="sidebar_search">
         <div className="sidebar_searchcontainer">
@@ -32,10 +32,10 @@ export default function Sidebar() {
         </div>
       </div>
       <div className="sidebar_chat">
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
-        <SidebarChat />
+        <SidebarChat name="pushkar" m="1" msg="hello" />
+        <SidebarChat name="ekta" m="2" />
+        <SidebarChat name="ankit" m="3" />
+        <SidebarChat name="golumolu" />
       </div>
     </div>
   );
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,18 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Avatar from "@material-ui/core/Avatar/Avatar";
-export default function SidebarChat() {
-  const [random, setrandom] = useState("");
+export default function SidebarChat({ name, m, msg }) {
+  const [random, setrandom] = useState("abc");
 
   useEffect(() => {
-    const number = Math.floor(Math.random() * 10);
-    setrandom(number);
+    var a = Math.floor(Math.random() * 100);
+    setrandom(a);
   }, []);
 
   return (
     <div>
       <div className="SidebarChat">
         <Avatar src={`https://avatars.dicebear.com/api/male/${random}.svg`} />
-        <button>submit</button>
+        <div className="SidebarChat_info">
+          <h1>{name}</h1>
+          <p>{m}</p>
+          <h2>{msg}</h2>
+        </div>
       </div>
     </div>
   );
